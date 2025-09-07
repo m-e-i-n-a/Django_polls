8b02267b9a7d19bc8fa0c0436cfee7e816b9320c
m-e-i-n-a <meina.h.0721@gmail.com>
2025-09-07 16:04:23 +0900

    vewsの編集

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("こんにちは")
+
+def hobby(request):
+    return HttpResponse("私の趣味は吹奏楽です。")
+
+def greet(request, name):
+    message = "こんにちは" + name + "さん"
+    return HttpResponse(message)
